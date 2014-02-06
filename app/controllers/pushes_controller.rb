81092d4f1b47c82e0a5376907431d08aac676366
Jan Dudek <jd@jandudek.com>
2014-02-07 00:41:34 +0100

    [#4] Added authentication for pushes

--- app/controllers/pushes_controller.rb
+++ app/controllers/pushes_controller.rb
@@ -1,8 +1,17 @@
 class PushesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
+  before_action :authenticate_push
 
   def create
     push = Push.new(JSON.parse(params[:payload])).create_commits
     head :ok
+  end
+
+  private
+
+  def authenticate_push
+    unless params[:secret].present? && params[:secret] == ENV["PUSH_SECRET"]
+      render nothing: true, status: 403
+    end
   end
 end
